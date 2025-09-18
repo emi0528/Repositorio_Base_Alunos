f1e81e24995f6871077ecb530b0f9ce107ab6456
emi0528 <emillyg.gusmao06@gmail.com>
2025-09-18 11:04:15 -0300

    Organização do Repositório

---
+++ Modulo 1/Loopings/while.py
@@ -0,0 +1,16 @@
+n = int(input("tabuada de:"))
+x = 1
+while x <= 10:
+    print(n,"x", "=", n*x)
+    x=x+1
+    ...
+    def wile00():
+        s=0
+        wile true:
+        v=int(input("digite un numero a somar ou 0 para sair
+         if v==0:
+             break
+         s = s+v
+     print(s)
+     while00()
+    
---
+++ Modulo 1/Loopings/while_com_lista.py
@@ -0,0 +1,10 @@
+L = []
+while true:
+    n = int(input("digiteum numero(0 sai):"))
+    if n == 0:
+        break
+    L.append(n)
+x=0
+wile x < len(L):
+    print(L[x])
+    x=x+1
